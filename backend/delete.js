c1091e3aa71537e8e4aa299430726345c9b165e3
michelmansur <michel.mansur@sou.inteli.edu.br>
2022-05-26 15:15:09 -0300

    delete.js

--- backend/delete.js
+++ backend/delete.js
@@ -1,8 +1,19 @@
+const express = require('express'); 
+const app = express();
+
+const hostname = 'localhost';
+const port = 1324;
+const sqlite3 = require('sqlite3').verbose(); 
+const DBPATH = 'dbRev.db'; 
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(express.static("../frontend/"));
+
 app.post('/delcad', urlencodedParser, (req, res) => {
     res.statusCode = 200;
     res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
   
-    sql = "DELETE FROM assistente WHERE email = " + req.body.IDcadastro;
+    sql = "DELETE FROM assistente WHERE email = " + req.body.senha;
     var db = new sqlite3.Database(DBPATH); // Abre o banco
     db.run(sql, [],  err => {
         if (err) {
@@ -13,3 +24,6 @@ app.post('/delcad', urlencodedParser, (req, res) => {
     db.close(); // Fecha o banco
   });
   
+  app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
