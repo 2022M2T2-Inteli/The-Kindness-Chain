446d1899713a9cab033c491da4c59ccf20e0d750
michelmansur <michel.mansur@sou.inteli.edu.br>
2022-05-26 14:36:43 -0300

    backend

---
+++ backend/delete.js
@@ -0,0 +1,15 @@
+app.post('/delcad', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+  
+    sql = "DELETE FROM assistente WHERE email = " + req.body.IDcadastro;
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    db.close(); // Fecha o banco
+  });
+  
