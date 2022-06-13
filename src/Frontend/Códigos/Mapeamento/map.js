3cf7162c214085602c363366ce2237c7e17d3acd
jeanroths <jean.machado@sou.inteli.edu.br>
2022-06-13 15:23:44 -0300

    Update map.js

--- src/Frontend/Códigos/Mapeamento/map.js
+++ src/Frontend/Códigos/Mapeamento/map.js
@@ -24,4 +24,28 @@ var map = {
                 });
             
         }
-    }}
+    },
+
+    update(IDcadastro, oldNomePessoa, oldTempoRua, oldLocalização, oldOutrasInfos) {
+
+        var nomePessoa = document.getElementById("nomePessoa").value.trim(oldNomePessoa);
+        var tempoRua = document.getElementById("tempoRua").value.trim(oldTempoRua);
+        var localização = document.getElementById("localização").value.trim(oldLocalização);
+        var outrasInfos = document.getElementById("outrasInfos").value.trim(oldOutrasInfos);
+        //var nomePessoa = prompt('Digite o novo nome:', oldTitle);
+        if (title) {
+            if (title.trim() != '') {
+                $.ajax({
+                    type: 'POST',
+                    url: api + '/userupdate',
+                    data: {nomePessoa: nomePessoa, tempoRua: tempoRua, localização: localização, outrasInfos: outrasInfos, IDcadastro: IDcadastro},
+                }).done(function () {
+                    users.list();
+                }).fail(function (msg) {
+                    //console.log('FAIL');
+                }).always(function (msg) {
+                    //console.log('ALWAYS');
+                });
+            }
+        }
+    }};
