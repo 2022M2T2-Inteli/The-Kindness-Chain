cdaa87a9684e94ec955239a2efc36b02d5050f3d
Lucas211-ctrl <lucas.vieira@sou.inteli.edu.br>
2022-06-18 20:18:49 -0300

    Back

--- src/Frontend/Códigos/cadastroAtendidos/cad.js
+++ src/Frontend/Códigos/cadastroAtendidos/cad.js
@@ -28,3 +28,68 @@ var cad = {
     },
 
 }
+
+
+var pessoas=[];//Vetor para armazenar os produtos
+function addPessoa(id,nome){//Metodo para adicionar os produtos no array
+  console.log("oi")
+  pessoas[pessoas.length] = {id:id,nome:nome};
+  console.log(pessoas);
+}
+
+//Exemplo adicionando os produtos
+const api = "http://localhost:1324"
+$.ajax({
+  url: api + '/usersassist',
+  crossDomain: true,
+  headers: {'Access-Control-Allow-Origin': '*' },
+  type: 'GET',
+  success: data => {
+    data.forEach(element => {
+      addPessoa(element.IDcadastro, element.nomeSocial);
+    });
+  }
+});
+
+$("#input").keypress(function(e){//Adiciona ação de actionkey no input
+  if(e.which == 13) {
+    buscar();
+  }
+});
+
+$("#buscar").click(function(){//Adiciona ação ao botão
+  buscar();
+});
+
+function buscar(){//Função responsavel de buscar os produtos
+  $('tbody').html("");
+  var p_listados=0;//Inicia contador de produtos encontrados
+  $('.pessoas').remove();//Limpa a lista de produtos
+  var txtbusca=$("#input").val().toLowerCase();//Captura texto digitado e converte para minusculo
+  //Percorre a lista de produtos e imprime ocorrencias verdadeiras
+  for(var i=0;i<pessoas.length;i++){
+    var nome=pessoas[i].nome.toLowerCase();
+    var cod=pessoas[i].id;
+    if(nome.match(txtbusca)){//Busca por nome
+      listarPessoas(i);
+      p_listados++;
+    }else if(txtbusca==cod){//Busca por código
+      listarPessoas(i);
+      p_listados++;
+    }
+  }
+  $('#p_listados').html(p_listados);//Mostra quantidade encontrada
+  $('#p_total').html(pessoas.length);//Mostra quantidade total de produtos cadastrados
+}
+ function listarPessoas(i){//Lista o produto do indece do array produto informado
+    if(i!=null){
+      //$('tbody').append("<tr class='produto'><td>"+pessoas[i].id+"</td><td>"+pessoas[i].nome+"</td><td>"+pessoas[i].tempoRua+"</td><td>"+pessoas[i].localizacao+"</td><td>"+pessoas[i].outrasInfo+"</td><td><button class='cadastrar'><i class='fa-solid fa-plus'></i></button></td><td><a href='mapUpdate.html' onclick='maps.list(" + pessoas[i].id + "," + pessoas[i].nome + "," + pessoas[i].tempoRua + "," + pessoas[i].localizacao + "," + pessoas[i].outrasInfo + ")'><button class='alterar'><i class='fa-solid fa-pen-to-square'></i></button></td><td><button class='deletar' onclick='user.delete(" + pessoas[i].id + ")'><i class='fa-solid fa-trash-can'></i></button></td></tr>");
+      //$('tbody').append("<tr class='produto'><td>"+pessoas[i].id+"</td><td>"+pessoas[i].nome+"</td><td>"+pessoas[i].tempoRua+"</td><td>"+pessoas[i].localizacao+"</td><td>"+pessoas[i].outrasInfo+"</td><td><button class='cadastrar'><i class='fa-solid fa-plus'></i></button></td><td><button class='alterar' onclick='maps.list(" + pessoas[i].id + ",\"" + pessoas[i].nome + ",\"" + pessoas[i].tempoRua + ",\"" + pessoas[i].localizacao + ",\"" + pessoas[i].outrasInfo + ")'><i class='fa-solid fa-pen-to-square'></i></button></td><td><button class='deletar' onclick='user.delete(" + pessoas[i].id + ")'><i class='fa-solid fa-trash-can'></i></button></td></tr>");    
+      //$('tbody').append("<tr class='produto'><td>"+pessoas[i].id+"</td><td>"+pessoas[i].nome+"</td><td>"+pessoas[i].tempoRua+"</td><td>"+pessoas[i].localizacao+"</td><td>"+pessoas[i].outrasInfo+"</td><td><button class='cadastrar'><i class='fa-solid fa-plus'></i></button></td><td><button class='alterar' onclick='maps.list()'><i class='fa-solid fa-pen-to-square'></i></button></td><td><button class='deletar' onclick='user.delete(" + pessoas[i].id + ")'><i class='fa-solid fa-trash-can'></i></button></td></tr>");    
+
+      $('tbody').append("<tr class='produto'><td>"+pessoas[i].id+"</td><td>"+pessoas[i].nome+"</td><td>"+pessoas[i].toalha+"</td><td><button class='cadastrar'><i class='fa-solid fa-plus'></i></button></td></tr>");
+    }
+  }
+
+
+buscar();//Inicializa mostrando todos os itens existentes
