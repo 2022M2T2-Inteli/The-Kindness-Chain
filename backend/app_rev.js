95526297be795be72bbed1272f5a4fa9a41c4276
Leao09 <99265654+Leao09@users.noreply.github.com>
2022-06-23 11:09:41 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/2022M2T2/Projeto3""
    This reverts commit e504dac986c8c75aa5d5c6a6e26ab43ed189d1a2.

--- backend/app_rev.js
+++ backend/app_rev.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const hostname = "localhost";
-const port = 1324;
+const port = 1234;
 const sqlite3 = require("sqlite3").verbose();
 const DBPATH = "dbRev.db";
 const bodyParser = require("body-parser");
@@ -58,16 +58,8 @@ app.post("/userupdate", urlencodedParser, (req, res) => {
   res.statusCode = 200;
   res.setHeader("Access-Control-Allow-Origin", "*"); // Isso é importante para evitar o erro de CORS
 
-  sql =
-    "UPDATE mapeamento SET nomePessoa = '" +
-    req.body.nomePessoa +
-    "', tempoRua = '" +
-    req.body.tempoRua +
-    "', outrasInfos = '" +
-    req.body.outrasInfos +
-    "', localização = '" +
-    req.body.localização;
-  "' WHERE IDcadastro = '" + req.body.IDcadastro + "'";
+  sql = "UPDATE mapeamento SET nomePessoa = '" + req.body.nomePessoa + "', tempoRua = '" + req.body.tempoRua + "', outrasInfos = '" + req.body.outrasInfos + "', localização = '" + req.body.localização + "' WHERE IDcadastro = '" + req.body.IDcadastro + "'";
+
   var db = new sqlite3.Database(DBPATH); // Abre o banco
   db.run(sql, [], (err) => {
     if (err) {
@@ -140,11 +132,7 @@ app.post("/userupdateof", urlencodedParser, (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // Isso é importante para evitar o erro de CORS
 
   sql =
-    "UPDATE atividades SET oficina  = '" +
-    req.body.oficina +
-    "' WHERE IDatv = '" +
-    req.body.IDatv +
-    "'";
+    "UPDATE atividades SET oficina  = '" + req.body.oficina + "' WHERE IDatv = '" + req.body.IDatv + "'";
   var db = new sqlite3.Database(DBPATH); // Abre o banco
   db.run(sql, [], (err) => {
     if (err) {
@@ -484,6 +472,9 @@ app.post("/educadordelete", urlencodedParser, (req, res) => {
   db.close(); // Fecha o banco
 });
 
+
+
+
 app.get("/getficha", (req, res) => {
   res.statusCode = 200;
   res.setHeader("Access-Control-Allow-Origin", "*"); // Isso é importante para evitar o erro de CORS
@@ -649,6 +640,33 @@ app.post("/voldelete", urlencodedParser, (req, res) => {
   });
   db.close(); // Fecha o banco
 });
+
+
+
+
+//Post------------------------------------------//
+// server.post('/getficha', (req, res) => {
+//  res.statusCode = 200;
+//  res.setHeader('Access-Control-Allow-Origin', '*');
+//   var data1 = req.body.data1
+//   console.log(data1)
+//   var data2 = req.body.data2
+//   console.log(data2)
+//   var db = new sqlite3.Database(DBPATH); // Abre o banco
+//   var sql = `SELECT SUM(qt_banho) qt_banho, SUM(nr_lanches) nr_lanches, SUM(qt_bazar) qt_bazar, COUNT(id_atendido) id_atendido FROM atendimentos WHERE dt_atendimento BETWEEN "${data1}" AND "${data2}"`;
+//   //usado as datas requeridas para consulta no banco de dados
+//   db.all(sql, [],  (err, rows ) => {
+//       if (err) {
+//           throw err;
+//       }
+//       res.json(rows);
+//   });
+//   db.close(); // Fecha o banco
+// });
+
+
+
+
 
 // app.listen(process.env.PORT,() => {
 //   console.log(`Server running`);
