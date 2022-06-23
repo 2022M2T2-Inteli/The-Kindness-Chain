b581e1b7e0673c7d39f6b2a58546bd350d97b03b
Gibeking <99186257+Gibeking@users.noreply.github.com>
2022-06-23 11:11:56 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/2022M2T2/Projeto3""
    This reverts commit d844792cd23a2f570caccd518fe2e4a76a020339.

--- src/Frontend/Códigos/páginaFichas/update.js
+++ src/Frontend/Códigos/páginaFichas/update.js
@@ -1,28 +1,62 @@
-function valor(){
-  alert("deu certo");
-}
+const api = "http://localhost:1324"
+
+console.log(decodeURI(location.search.substring()));
+
+var queryString = decodeURI(location.search.substring().replace("?","")).split("|");
+console.log(queryString);
+window.onload = function() {
+  upd.list(queryString[0], queryString[1], queryString[2], queryString[3], queryString[4])
+};
+
 var upd = {
     list(id,oldNome,oldTempoRua,oldLocalizacao,oldOutrasInfos) {
-      alert("deu certo")
+        console.log(oldTempoRua);//,oldNome,oldTempoRua,oldLocalizacao,oldOutrasInfos);
+        document.querySelector('#info1').innerHTML += '<input id="nomePessoa" type="text" class="login__input" placeholder= "Nome" value=\"'+ oldNome +'\" name="nome">';
+        document.querySelector('#info2').innerHTML += '<input id="tempoRua" type="text" class="login__input" placeholder= "Tempo de Rua" value =\" '+ oldTempoRua +'\" name="tempo">';
+        document.querySelector('#info3').innerHTML += '<input id="localizacao" type="text" class="login__input" placeholder= "Localização" value =\" '+oldLocalizacao+'\" name="local">'; 
+        document.querySelector('#info4').innerHTML += '<input id="outrasInfos" type="text" class="login__input" placeholder= "Outras Informações" value = \"'+ oldOutrasInfos +'\" name="info">';
+        document.querySelector('#inf').innerHTML += '<button onclick="dadosNovos('+ id +')" class="button login__submit "><span class="button__text">Cadastrar</span></button>	';
         $.ajax({
-            //url: api + '/users',
+            url: api + '/users',
             type: 'GET',
             success: data => {
-              alert("deu certo");
-              console.log("Deu certo");
-              console.log(id);
-              console.log(oldNome);
-              console.log(oldTempoRua);
-              console.log(oldLocalizacao);
-              console.log(oldOutrasInfos);
-              //document.querySelector('#info1').innerHTML += '<input id="nomePessoa" type="text" class="login__input" placeholder= '+ oldNome +' value='+ oldNome +' name="nome">';
-              //document.querySelector('#info2').innerHTML += '<input id="tempoRua" type="text" class="login__input" placeholder= '+ oldTempoRua +' value = '+ oldLocalizacao +'name="tempo">';
-              //document.querySelector('#info3').innerHTML += '<input id="localização" type="text" class="login__input" placeholder= '+oldLocalizacao+' value = '+oldLocalizacao+'name="local">'; 
-              //document.querySelector('#info4').innerHTML += '<input id="outrasInfos" type="text" class="login__input" placeholder= '+ oldOutrasInfos +' value = '+ oldOutrasInfos +' name="info">';
+              //document.querySelector('#info1').innerHTML += '<input id="nomePessoa" type="text" class="login__input" placeholder= "Nome" value='+ oldNome +' name="nome">';
+              //document.querySelector('#info2').innerHTML += '<input id="tempoRua" type="text" class="login__input" placeholder= "Tempo de Rua" value = '+ oldTempoRua +'name="tempo">';
+              //document.querySelector('#info3').innerHTML += '<input id="localização" type="text" class="login__input" placeholder= "Localização" value = '+oldLocalizacao+'name="local">'; 
+              //document.querySelector('#info4').innerHTML += '<input id="outrasInfos" type="text" class="login__input" placeholder= "Outras Informações" value = '+ oldOutrasInfos +' name="info">';
             }
         });
         
     }
-    
   };
-  
+
+function dadosNovos(id){
+  alert("dados corretos");
+  var id = Number(id);
+  var nome = document.getElementById("nomePessoa").value;
+  var tempoRua = Number(document.getElementById("tempoRua").value);
+  var local = Number(document.getElementById("localizacao").value);
+  var outrasInfos = document.getElementById("outrasInfos").value;
+  up.update(id, nome, tempoRua, local, outrasInfos);
+}
+
+var up = {
+    update(id, nome, tempoRua, local, outrasInfo) {
+      alert(id + nome + tempoRua + local, outrasInfo)
+      alert("ola");
+      if (id && nome && tempoRua && local && outrasInfo) {
+              $.ajax({
+                  type: 'POST',
+                  url: api + '/userupdate',
+                  data: {nomePessoa: nome, tempoRua: tempoRua, outrasInfos: outrasInfo, localização: local, IDcadastro: id},
+              }).done(function () {
+                  window.location.assign("lista.html")
+                  users.list();
+              }).fail(function (msg) {
+                  //console.log('FAIL');
+              }).always(function (msg) {
+                  //console.log('ALWAYS');
+              });
+      }
+  },
+}
